8605fcc0efe6332b2c8ab43d0c4358201b35a9b2
mohamed mostafa elsayed hella <hoomos98@gmail.com>
2023-04-04 13:25:13 +0200

    END OF COURSE AND AFTER ADING ENV FILE

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
 
 // MongoDB connection String
-const dbURI = "mongodb+srv://netninja:test1234@nodetuts.4htdrgx.mongodb.net/node-tuts?retryWrites=true&w=majority";
+const dbURI = process.env.CONNECTION_STRING;
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result)=>{
         // listen for request
@@ -22,55 +22,10 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 // register view engine (configer some application setting)
 app.set('view engine', 'ejs');
 
-// not required in this case because it is the same name but if you 
-// want to change it to other folder .. for example myViews
-// app.set('views', 'myViews'); 
-app.set('views', 'views'); 
-
 // middleware & static files
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'))
-
-// // mongoose and mongo sandbox routes   => For testing
-// app.get('/add-blog', (req, res)=>{
-//     const blog = new Blog({
-//         title: 'new blog',
-//         snippet: 'about new blog',
-//         body: 'more about my new blog'
-//     });
-
-//     blog.save()
-//         .then((result)=>{
-//             console.log('blog saved');
-//             res.send(result);
-//         })
-//         .catch((err)=>{
-//             console.log(err);
-//         });
-// })
-
-// app.get('/all-blogs', (req, res)=>{
-//     Blog.find()
-//         .then((result)=>{
-//             res.send(result);
-//         })
-//         .catch((err)=>{
-//             console.log(err);
-//         });
-// });
-
-// app.get('/single-blog', (req, res)=>{
-//     Blog.findById('642ba2ddf3334d366aa18117')
-//         .then((result)=>{
-//             res.send(result);
-//         })
-//         .catch((err)=>{
-//             console.log(err);
-//         });
-// })
-
-
 
 
 app.get('/', (req, res)=>{
@@ -83,7 +38,7 @@ app.get('/about', (req, res)=>{
 })
 
 // blog routes
-app.use(blogRoutes);
+app.use('/blogs',blogRoutes);
 
 // redirects
 app.get('/about-us', (req, res)=>{
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,63 +1,19 @@
 const express = require('express');
-const Blog = require('../models/blog');
-
+const blogController = require('../controllers/blogController');
 
 const route = express.Router();
 
 
 // blog routs
-route.get('/blogs', (req, res)=>{
-    Blog.find().sort({createdAt: -1})
-        .then((result)=>{
-            res.render('index' , {title : 'All Blogs', blogs:result})
-        })
-        .catch((err)=>{
-            console.log(err); 
-        });
-})
+route.get('/', blogController.blog_index)
 
-route.post('/blogs', (req, res)=>{
-    // console.log(req.body);  // => to be req.body we need to use this middleware (route.use(express.urlencoded({ extended: true}));) 
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result)=>{
-            res.redirect('/blogs')
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
-});
+route.post('/', blogController.blog_create_bost);
 
+route.get('/create', blogController.blog_create_get)
 
-route.get('/blogs/create', (req, res)=>{
-    res.render('create' , {title : 'Create a new Blog'});
-})
+route.get('/:id', blogController.blog_details);
 
-
-route.get('/blogs/:id',(req, res)=>{
-    const id =  req.params.id;
-    Blog.findById(id)
-        .then((result)=>{
-            res.render('details',{title : 'Blog Details', blogs:result})
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
-});
-
-route.delete('/blogs/:id',(req, res)=>{
-    const id =  req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then((result)=>{
-            // res.redirect('/blogs');
-            console.log('deleted => ' , result)
-            res.json({ redirect: '/blogs'});
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
-});
-
+route.delete('/:id', blogController.blog_delete);
 
 
 module.exports = route;
