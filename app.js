2b6a7898f933016a8df87d2d8c0da26af481faec
mohamed mostafa elsayed hella <hoomos98@gmail.com>
2023-04-04 11:39:47 +0200

    mongo sandBox

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
 
-// register view engine (configer some application setting)
+// MongoDB connection String
+const dbURI = "mongodb+srv://netninja:test1234@nodetuts.4htdrgx.mongodb.net/node-tuts?retryWrites=true&w=majority";
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result)=>{
+        // listen for request
+        app.listen(3000); // we move the listen function her becouse it is loggical no response with out connection to the databse.
+        console.log("connected to DB");
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
 
+// register view engine (configer some application setting)
 app.set('view engine', 'ejs');
 
 // not required in this case because it is the same name but if you 
@@ -13,57 +27,63 @@ app.set('view engine', 'ejs');
 // app.set('views', 'myViews'); 
 app.set('views', 'views'); 
 
-// listen for request
-app.listen(3000);
-
-// custome middleware 
-// app.use((req, res, next)=>{
-//     console.log('New requst made.');
-//     console.log('host: ', req.hostname);
-//     console.log('path: ', req.path);
-//     console.log('path: ', req.method);
-//     next();
-// });
-// app.use((req, res, next)=>{
-//     console.log('in the next middleware...');
-//     next();
-// });
-
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(morgan('dev'))
+
+// // mongoose and mongo sandbox routes   => For testing
+// app.get('/add-blog', (req, res)=>{
+//     const blog = new Blog({
+//         title: 'new blog',
+//         snippet: 'about new blog',
+//         body: 'more about my new blog'
+//     });
+
+//     blog.save()
+//         .then((result)=>{
+//             console.log('blog saved');
+//             res.send(result);
+//         })
+//         .catch((err)=>{
+//             console.log(err);
+//         });
+// })
+
+// app.get('/all-blogs', (req, res)=>{
+//     Blog.find()
+//         .then((result)=>{
+//             res.send(result);
+//         })
+//         .catch((err)=>{
+//             console.log(err);
+//         });
+// });
+
+// app.get('/single-blog', (req, res)=>{
+//     Blog.findById('642ba2ddf3334d366aa18117')
+//         .then((result)=>{
+//             res.send(result);
+//         })
+//         .catch((err)=>{
+//             console.log(err);
+//         });
+// })
 
 
-// morgan middleware
-app.use(morgan('dev'));
-app.use(morgan('tiny'));
+
 
 app.get('/', (req, res)=>{
-
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defest bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'make me flay to the moon', snippet: 'Lorem ipsum dolor sit amet consectetur'}
-    ];
-
-    res.render('index' , {title : 'Home', blogs}); 
+    res.redirect('/blogs')
 })
 
-// custome middleware
-// this medilewae will not run if the request path was ('/') because
-// the request will end in the get function above it and will not reatch her
-// app.use((req, res, next)=>{
-//     console.log('in the next middleware...');
-//     next();
-// });
 
 app.get('/about', (req, res)=>{
     res.render('about' , {title : 'About'});
 })
 
-app.get('/blogs/create', (req, res)=>{
-    res.render('create' , {title : 'Create a new Blog'});
-})
+// blog routes
+app.use(blogRoutes);
 
 // redirects
 app.get('/about-us', (req, res)=>{
